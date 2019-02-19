45a7b4d6501863018a755f0aad5fe8249a3a470c
Arjun Muhunthan <arjun212@hotmail.co.uk>
2019-02-18 19:22:45 -0500

    spacing

--- Cpp/GC/main.cpp
+++ Cpp/GC/main.cpp
@@ -19,17 +19,13 @@ namespace
     double computeGc( const std::string & dna )
     {
         if( dna.empty() )
-        {
             return -1 ;
-        }
 
         int gcCount = 0 ;
 
         for( char c : dna )
-        {
             if( c == 'G' || c == 'C' )
                 ++gcCount ;
-        }
 
         return ( gcCount * 100.0 ) / dna.length() ;
     }
@@ -52,7 +48,6 @@ namespace
 
 int main()
 {
-
     std::vector< std::string > lines( Utils::ReadFile( "./data" ) ) ;
 
     std::string currentGcMarker = NULL_MARKER ;
@@ -61,19 +56,18 @@ int main()
     double      highestGC       = -1          ;
     std::string highestGCMarker = NULL_MARKER ;
 
-    // for( const std::string & line : lines ) is the same
-    for( std::vector< std::string >::const_iterator it = lines.cbegin() ,
-                                                    et = lines.cend()   ; it != et ; ++it )
+    for( const std::string & line : lines )
     {
-        const std::string & line = *it;
-
         std::size_t pos = line.find( '>' ) ;
 
         if( pos != std::string::npos ) 
         {
             currentGcMarker = getMarker( line ) ;
-            propagateHighestGc( currentGcMarker, buffer, highestGC, highestGCMarker ) ;
-            
+            propagateHighestGc( currentGcMarker ,
+                                buffer          ,
+                                highestGC       ,
+                                highestGCMarker ) ;
+
             buffer = "" ;
         }
         else
@@ -83,9 +77,13 @@ int main()
 
     }
 
-    propagateHighestGc( currentGcMarker, buffer, highestGC, highestGCMarker ) ;
+    propagateHighestGc( currentGcMarker ,
+                        buffer          ,
+                        highestGC       ,
+                        highestGCMarker ) ;
 
-    std::cout << "Rosalind_" << highestGCMarker << "\n" << highestGC << "\n" ;
+    std::cout << "Rosalind_" << highestGCMarker << "\n"
+                             << highestGC       << "\n" ;
 
 
     return 0 ;
